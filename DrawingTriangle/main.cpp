b9f2f25a344e0bdee7c78faec6869bd050e52635
to_matih <maatteeoh@wp.pl>
2022-05-27 18:27:40 +0100

    Handling minimisation

--- DrawingTriangle/main.cpp
+++ DrawingTriangle/main.cpp
@@ -1009,6 +1009,13 @@ private:
     }
 
     void recreateSwapChain(){
+        int width = 0, height=0;
+        glfwGetFramebufferSize(window, &width, &height);
+        while (width==0 || height==0) {
+            glfwGetFramebufferSize(window, &width, &height);
+            glfwWaitEvents();
+        }
+
         vkDeviceWaitIdle(device);
 
         cleanupSwapChain();
