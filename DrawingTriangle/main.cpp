061a549275ea4b602ca8a0a862c0d38d426bacda
to_matih <maatteeoh@wp.pl>
2022-06-06 23:04:52 +0100

    Synchronisation fix

--- DrawingTriangle/main.cpp
+++ DrawingTriangle/main.cpp
@@ -1483,6 +1483,7 @@ private:
 
         VkPipelineStageFlags sourceStage;
         VkPipelineStageFlags destinationStage;
+        VkCommandBuffer barrieredCommandBuffer;
 
         if(oldLayout == VK_IMAGE_LAYOUT_UNDEFINED && newLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL){
             barrier.srcAccessMask = 0;
@@ -1490,19 +1491,23 @@ private:
 
             sourceStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
             destinationStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
+
+            barrieredCommandBuffer = setupCommandBuffers[0];
         }
         else if(oldLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL && newLayout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL){
             barrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
             barrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
 
             sourceStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
-            destinationStage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+            destinationStage = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT;
+
+            barrieredCommandBuffer = setupCommandBuffers[1];
         }
         else{
             throw std::runtime_error("unsuportted layout transition!");
         }
 
-        vkCmdPipelineBarrier(setupCommandBuffers[0], 
+        vkCmdPipelineBarrier(barrieredCommandBuffer, 
             sourceStage, destinationStage, 
             0, 
             0, nullptr, 
